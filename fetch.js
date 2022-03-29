1b9a285dd56ce0ec6d44701f13c971e8b31b8994
damola <egbebitimi121@gmail.com>
2022-03-29 01:48:06 -0800

    started the UI

---
+++ fetch.js
@@ -0,0 +1,11 @@
+
+
+// fetch('https://data-imdb1.p.rapidapi.com/titles/search/keyword/deadpool', {
+// 	method: 'GET',
+// 	headers: {
+// 		'X-RapidAPI-Host': 'data-imdb1.p.rapidapi.com',
+//     	'X-RapidAPI-Key': '2d39e8dc5dmshb63147aa42b6c97p127e1bjsnf94f3a9f0c41'
+// 	}
+// })
+// 	.then(res => res.json())
+// 	.then(res => console.log(res))
