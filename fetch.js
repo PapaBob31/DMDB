22538f59808caa8ee89b0544da210db8744bbab1
damola <egbebitimi121@gmail.com>
2022-03-30 22:35:21 -0800

    added some new features but still on the UI

--- fetch.js
+++ fetch.js
@@ -1,3 +1,29 @@
+let sectionNamesContainer = document.getElementById("section-names")
+let sectionNames = Array.from(sectionNamesContainer.querySelectorAll("div"))
+let sections = document.querySelectorAll('.sections')
+sections = Array.from(sections)
+
+for(let i=0; i<sectionNames.length; i++) {
+	sectionNames[i].addEventListener("click", changeSection)
+}
+
+function changeSection() {
+	let clickedSection = this
+	let currentSection = document.getElementById("current-section")
+	if (clickedSection != currentSection) {
+		currentSection.removeAttribute('id', 'current-section')
+		clickedSection.setAttribute('id', 'current-section')
+	}
+	let newIndex = sectionNames.indexOf(clickedSection)
+	let oldIndex = sectionNames.indexOf(currentSection)
+	sections[oldIndex].style.display = 'none'
+	if (newIndex != 0) {
+		sections[newIndex].style.display = 'flex'
+	}else {
+		sections[newIndex].style.display = 'initial'
+	}
+}
+
 
 
 // fetch('https://data-imdb1.p.rapidapi.com/titles/search/keyword/deadpool', {
