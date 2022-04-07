4db8486bd75090931ff3c17b40cbb51f4c805b0d
damola <egbebitimi121@gmail.com>
2022-04-07 15:07:28 -0800

    added alot of new features

--- fetch.js
+++ fetch.js
@@ -1,13 +1,62 @@
+let menuIcon = document.getElementById("mobile-menu")
+let menuBar = document.getElementById("menu-bar")
+let closeMenu = document.getElementById("close-menu")
+
+menuIcon.addEventListener("click", showMenu)
+closeMenu.addEventListener("click", hideMenu)
+
+let menuOnscreen = false
+
+function showMenu() {
+	if (!menuOnscreen) {
+		menuBar.style.left = "0"
+		menuOnscreen = true
+	}
+}
+
+function hideMenu() {
+	if (menuOnscreen) {
+		menuBar.style.left = "-100%"
+		menuOnscreen = false
+	}
+}
+
+let genresLink = document.getElementById("genres-link")
+let genresContainer = document.getElementById("genres")
+let genres = genresContainer.querySelector("div")
+
+function showGenres() {
+	genresContainer.style.left = '0'
+	genres.style.transform = 'scaleY(100%)'
+}
+
+function hideGenres() {
+	genres.style.transform = 'scaleY(0)'
+	genresContainer.style.left = '-100%'
+}
+
+genresLink.addEventListener("click", showGenres)
+
+let backArrow = document.getElementById("back-arrow")
+backArrow.addEventListener("click", hideGenres)
 
 let vel = 0
 let navLeft = document.getElementById("move-left")
 let navRight = document.getElementById("move-right")
+let bottomStatus = document.getElementById("bottom-status")
+let status = bottomStatus.querySelectorAll("div")
+status = Array.from(status)
+statusIndex = 0
+
 function moveImagesLeft() {
 	if (vel != -200) {
 		vel -= 100
 		for (let i=0; i<trendingMovies.length; i++) {
 			trendingMovies[i].style.transform = `translateX(${vel}%)`
 		}
+		status[statusIndex].removeAttribute("id")
+		statusIndex += 1
+		status[statusIndex].id = "current"
 	}
 }
 
@@ -17,6 +66,9 @@ function moveImagesRight() {
 		for (let i=0; i<trendingMovies.length; i++) {
 			trendingMovies[i].style.transform = `translateX(${vel}%)`
 		}
+		status[statusIndex].removeAttribute("id")
+		statusIndex -= 1
+		status[statusIndex].id = "current"
 	}
 }
 
@@ -58,36 +110,57 @@ function getTrendingMovies(list) {
 
 function getTrendingMoviesDetails(mediaList) {
 	let movieNo = 0
-	for (let i=0; i<mediaList.length; i++){
-		if (mediaList[i].type == "movie") {
-			fetch(`https://api.themoviedb.org/3/movie/${mediaList[i].id}?api_key=${key}&language=en-US&append_to_response=videos,release_dates`)
-			.then(response => response.json())
-			.then(response => {trendingMoviesDetails.push(response); updateTrendingMovies(trendingMoviesDetails, movieNo); movieNo += 1})
-		}else if (mediaList[i].type == "tv") {	
-			fetch(`https://api.themoviedb.org/3/tv/${mediaList[i].id}?api_key=${key}&language=en-US&append_to_response=videos,release_dates`)
-			.then(response => response.json())
-			.then(response => {trendingMoviesDetails.push(response); updateTrendingMovies(trendingMoviesDetails, movieNo); movieNo += 1})	
-		}
+	for (let i=0; i<mediaList.length; i++) {
+		fetch(`https://api.themoviedb.org/3/${mediaList[i].type}/${mediaList[i].id}?api_key=${key}&language=en-US&append_to_response=videos,release_dates`)
+		.then(response => response.json())
+		.then(response => {trendingMoviesDetails.push(response); updateTrendingMovies(trendingMoviesDetails, movieNo); movieNo += 1})
 	}
 }
 
 function updateTrendingMovies(details, i) {
-	movieName = trendingMovies[i].querySelector("#movie-name")
-	movieImage = trendingMovies[i].querySelector("img")
-	genres = trendingMovies[i].querySelector("#genres")
+	console.log(details[i])
+	mediaName = trendingMovies[i].querySelector("#media-name")
+	poster  = trendingMovies[i].querySelector("img")
+	genres = trendingMovies[i].querySelector("#trending-genres")
+	trailer = trendingMovies[i].querySelector("iframe")
 	overview = trendingMovies[i].querySelector("#overview")
-	certification = trendingMovies[i].querySelector("#certification")
-	runTime = trendingMovies[i].querySelector("#run-time")
+	releaseDateandType = trendingMovies[i].querySelector("#media-type-nd-release-date")
+	releaseDate = document.createElement("div")
+	mediaType = document.createElement("div")
+	poster.src = `https://image.tmdb.org/t/p/w1280${details[i].backdrop_path}`
 	if (details[i].original_name) {
-		movieName.textContent = details[i].original_name
+		mediaName.textContent = details[i].original_name
+		releaseDate.textContent = `first episode date: ${details[i].first_air_date}`
+		mediaType.textContent = "Tv Series"
+		releaseDateandType.appendChild(releaseDate)
+		releaseDateandType.appendChild(mediaType)
 	}else {
-		movieName.textContent = details[i].original_title
-		//certification.textContent = details[i].release_dates[41].release_dates[0].certification
+		mediaName.textContent = details[i].original_title
+		releaseDate.textContent = `first episode date: ${details[i].release_date}`
+		mediaType.textContent = "Movie"
+		releaseDateandType.appendChild(releaseDate)
+		releaseDateandType.appendChild(mediaType)
 	}
-	movieImage.src = `https://image.tmdb.org/t/p/w1280${details[i].backdrop_path}`
-	// genres = 
-	overview.textContent = details[i].overview
-	runTime.textContent = `Run Time: ${details[i].runtime} min`
+	for (let v = 0; v < details[i].videos.results.length; v++) {
+		if (details[i].videos.results[v].name == "Official Trailer") {
+			trailer.src=`https://www.youtube.com/embed/${details[i].videos.results[v].key}`
+			break
+		}
+	}
+
+	for (let n=0; n<details[i].genres.length; n++) {
+		genre = document.createElement("div")
+		genre.textContent = details[i].genres[n].name
+		genres.appendChild(genre)
+	}
+	overview.textContent = trimText(details[i].overview)
+}
+
+function trimText(text) {
+	if (text.length > 200) {
+		text = text.slice(0, 200)
+		return text
+	}
 }
 
 fetch(`https://api.themoviedb.org/3/trending/all/day?api_key=${key}`)
@@ -140,56 +213,92 @@ for (let i=0; i<movies.length; i++) {
 
 closeBtn.addEventListener("click", closeFullDetails)
 
+topPicks = document.getElementById("top-picks")
 
-// fetch(`https://api.themoviedb.org/3/movie/406759?api_key=${key}&language=en-US&append_to_response=videos,release_dates`)
-// 	.then(response => response.json())
-// 	.then(response => console.log(response))
+movieGenres = [] 
+tvGenres = []
 
-// fetch('https://api.themoviedb.org/3/movie/popular?api_key=${key}', {
-// 	method: 'GET',
-// 	headers: {'Authorization': 'Bearer ${key}'}
-// }).then(res => res.json())
-//   .then(res => console.log(res))
+// fetch(`https://api.themoviedb.org/3/genre/movie/list?api_key=${key}&language=en-US`)
+// .then(res => res.json())
+// .then(res => {movieGenres=res; console.log(movieGenres)})
 
-// fetch('https://api.themoviedb.org/3/movie/upcoming?api_key=${key}&language=en-US&page=1', {
-// 	method: 'GET',
-// 	headers: {'Authorization': 'Bearer ${key}'}}
-// ).then(res => res.json())
-//   .then(res => console.log(res))
+// fetch(`https://api.themoviedb.org/3/genre/tv/list?api_key=${key}&language=en-US`)
+// .then(res => res.json())
+// .then(res => {tvGenres=res; console.log(tvGenres)})
 
-//https://api.themoviedb.org/3/tv/{tv_id}?api_key=${key}&language=en-US130392
+// fetch(`https://api.themoviedb.org/3/movie/popular?api_key=${key}&page=1`)
+// .then(res => res.json())
+// .then(res => displayResponse(res, "movie", "top-movies"))
 
-// backdrop_path: "/64a8imymtJ4WOzIeyUHLtZnJ3wv.jpg"
-// first_air_date: "2022-03-30"
-// genre_ids: (4) [10759, 10765, 9648, 18]
-// id: 92749
-// media_type: "tv"
-// name: "Moon Knight"
-// origin_country: ['US']
-// original_language: "en"
-// original_name: "Moon Knight"
-// overview: "When Steven Grant, a mild-mannered gift-shop employee, becomes plagued with blackouts and memories of another life, he discovers he has dissociative identity disorder and shares a body with mercenary Marc Spector. As Steven/Marc’s enemies converge upon them, they must navigate their complex identities while thrust into a deadly mystery among the powerful gods of Egypt."
-// popularity: 523.123
-// poster_path: "/zQSABH2Dza4mXLow2f0V2IQvJOL.jpg"
-// vote_average: 8.4
-// vote_count: 65
-// [[Prototype]]: Object
-// 1:
-// adult: false
-// backdrop_path: "/lRbDyjI7HEaXxflFQbYpqHRGFBJ.jpg"
-// genre_ids: (4) [80, 18, 9648, 53]
-// id: 505026
-// media_type: "movie"
-// original_language: "en"
-// original_title: "Death on the Nile"
-// overview: "Belgian sleuth Hercule Poirot boards a glamorous river steamer with enough champagne to fill the Nile. But his Egyptian vacation turns into a thrilling search for a murderer when a picture-perfect couple’s idyllic honeymoon is tragically cut short."
-// popularity: 1152.285
-// poster_path: "/kVr5zIAFSPRQ57Y1zE7KzmhzdMQ.jpg"
-// release_date: "2022-02-09"
-// title: "Death on the Nile"
-// video: false
-// vote_average: 6.6
-// vote_count: 804
+// fetch(`https://api.themoviedb.org/3/tv/popular?api_key=${key}&page=1`)
+// .then(res => res.json())
+// .then(res => displayResponse(res, "tv", "top-tv"))
+
+// // fetch(`https://api.themoviedb.org/3/movie/now_playing?api_key=${key}&language=en-US&page=1&region=US`)
+// // .then(res => res.json())
+// // .then(res => console.log(res))
+
+// fetch(`https://api.themoviedb.org/3/movie/now_playing?api_key=${key}&language=en-US&page=1&region=US`)
+// .then(res => res.json())
+// .then(res => displayResponse(res, "movie", "recent-releases"))
+
+// fetch(`https://api.themoviedb.org/3/tv/on_the_air?api_key=${key}&language=en-US&page=1&region=US`)
+// .then(res => res.json())
+// .then(res => displayResponse(res, "tv", "recent-releases"))
+
+// fetch(`https://api.themoviedb.org/3/movie/upcoming?api_key=${key}&language=en-US&page=1&region=US`)
+// .then(res => res.json())
+// .then(res => displayResponse(res, "movie", "coming-soon"))
+
+function displayResponse(response, responseType, parentId) {
+	for (let i = 0; i < response.results.length; i++) {
+		let mediaContainer = document.createElement("div")
+		let poster = document.createElement("img")
+		let name = document.createElement("div")
+		let littleDetails = document.createElement("div")
+		let releaseYear = document.createElement("span")
+		let genresList = []
+		let mediaGenres = " | "
+		mediaContainer.className = "media-container"
+		name.id = "name"
+		littleDetails.id = "little-details"
+		poster.src = poster.src = `https://image.tmdb.org/t/p/w500${response.results[i].poster_path}`
+		if (responseType == "movie") {
+			name.textContent = response.results[i].title
+			genresList = movieGenres.genres
+			releaseYear.textContent = response.results[i].release_date.slice(0, 4)
+		}else if (responseType == "tv") {
+			name.textContent = response.results[i].name
+			genresList = tvGenres.genres
+			releaseYear.textContent = response.results[i].first_air_date.slice(0, 4)
+		}
+		for (let n=0; n < response.results[i].genre_ids.length; n++) {
+			for (let e=0; e < genresList.length; e++) {
+				if (response.results[i].genre_ids[n] == genresList[e].id) {
+					if (response.results[i].genre_ids[n] != response.results[i].genre_ids[-1]) {
+						mediaGenres += genresList[e].name + ', '
+						break
+					}else {
+						mediaGenres += genresList[e].name
+						break
+					}
+				}
+			} 
+		}
+		let parentContainer = document.getElementById(parentId)
+		let genres = document.createTextNode(mediaGenres)
+		littleDetails.appendChild(releaseYear)
+		littleDetails.appendChild(genres)
+		mediaContainer.appendChild(poster)
+		mediaContainer.appendChild(name)
+		mediaContainer.appendChild(littleDetails)
+		parentContainer.appendChild(mediaContainer)
+	}
+}
+
+
+// https://api.themoviedb.org/3/tv/{tv_id}?api_key=${key}&language=en-US130392
+
 
 //search-syntax: https://api.themoviedb.org/3/search/movie?api_key=${key}&query=deadpool&page=1
 //img-syntax: 'https://image.tmdb.org/t/p/w500/1g0dhYtq4irTY1GPXvft6k4YLjm.jpg'
